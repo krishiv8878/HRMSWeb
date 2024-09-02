57f75ec83b64ff023015a181e3b26fa8da701433
nidhi prajapati <nidhi.prajapati@krishivinnovations.com>
2024-09-02 11:16:25 +0530

    initial project structure changes

--- hrms-web-app/src/app/app.component.ts
+++ hrms-web-app/src/app/app.component.ts
@@ -6,11 +6,12 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 // import { HomeComponent } from "./components/home/home.component";
 import { HeaderComponent } from './component/header/header.component';
 import { SidbarComponent } from './component/sidbar/sidbar.component';
+import { LoginComponent } from "./auth/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatDividerModule, MatSidenavModule, CommonModule, HeaderComponent, SidbarComponent],
+  imports: [RouterOutlet, MatDividerModule, MatSidenavModule, CommonModule, HeaderComponent, SidbarComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
