03e56c51309b4e4c125c064d0ece8742d5f88497
nidhiprajapati11 <nidhiprajapati1111@gmail.com>
2025-01-03 11:33:01 +0530

    add new changes

--- hrms-web-app/src/app/component/skill/skill.component.ts
+++ hrms-web-app/src/app/component/skill/skill.component.ts
@@ -29,16 +29,16 @@ export class SkillComponent {
   toaster = inject(ToastrService)
 
   public columnDefs: ColDef[] = [
-    { field: "id", floatingFilter: true, filter: true, flex: 1 },
-    { field: "skillName", floatingFilter: true, filter: true, flex: 1 },
-    { field: "updatedBy", floatingFilter: true, filter: true, flex: 1 },
-    { field: "updatedDate", floatingFilter: true, filter: true, flex: 1 },
-    { field: "createdBy", floatingFilter: true, filter: true, flex: 1 },
-    { field: "createdDate", floatingFilter: true, filter: true, flex: 1 },
-    { field: "isActive", flex: 1, cellRenderer: (params: ICellRendererParams) => params.value ? `<i class="fa-solid fa-toggle-on" style="color: green; font-size: x-large;"></i>` : `'<i class="fa-solid fa-toggle-off" style="font-size: x-large; color: red; "></i>` },
-    // { field: "isActive", flex: 1, cellRenderer: TogglebuttonComponent },
+    { field: "id", floatingFilter: true, filter: true, },
+    { field: "skillName", floatingFilter: true, filter: true, },
+    { field: "updatedBy", floatingFilter: true, filter: true, },
+    { field: "updatedDate", floatingFilter: true, filter: true, },
+    { field: "createdBy", floatingFilter: true, filter: true, },
+    { field: "createdDate", floatingFilter: true, filter: true, },
+    { field: "isActive",  cellRenderer: (params: ICellRendererParams) => params.value ? `<i class="fa-solid fa-toggle-on" style="color: green; font-size: x-large;"></i>` : `'<i class="fa-solid fa-toggle-off" style="font-size: x-large; color: red; "></i>` },
+    // { field: "isActive",  cellRenderer: TogglebuttonComponent },
 
-    { field: "action", flex: 1, cellRenderer: ActionComponent, cellRendererParams: { Edit: this.Edit.bind(this), Delete: this.Delete.bind(this) } }
+    { field: "action",  cellRenderer: ActionComponent, cellRendererParams: { Edit: this.Edit.bind(this), Delete: this.Delete.bind(this) } }
   ]
 
   constructor() { this.columnDefs }
