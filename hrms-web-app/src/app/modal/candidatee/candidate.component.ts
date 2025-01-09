a404aec97f59b7a49f07adf09c7aeea9e99f24e1
nidhiprajapati11 <nidhiprajapati1111@gmail.com>
2025-01-09 12:11:54 +0530

    change modal data

--- hrms-web-app/src/app/modal/candidatee/candidate.component.ts
+++ hrms-web-app/src/app/modal/candidatee/candidate.component.ts
@@ -11,11 +11,12 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MAT_DIALOG_DATA, MatDialogClose, MatDialogRef } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
-import { MatSelectModule } from '@angular/material/select';
+import {MatSelectModule} from '@angular/material/select';
 
 @Component({
   selector: 'app-candidate',
   standalone: true,
+  imports: [MatInputModule, MatFormField, MatButtonModule, ReactiveFormsModule, MatRadioModule, CommonModule, MatCheckboxModule, MatDatepickerModule, MatNativeDateModule, MatDialogClose, MatSelectModule],
   imports: [MatInputModule, MatFormField, MatButtonModule, ReactiveFormsModule, MatRadioModule, CommonModule, MatCheckboxModule, MatDatepickerModule, MatNativeDateModule, MatDialogClose, MatSelectModule],
   templateUrl: './candidate.component.html',
   styleUrl: './candidate.component.scss'
