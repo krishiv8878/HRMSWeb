5cf6e25aede979c90e068fcbd4a672aa9ad29602
nidhiprajapati11 <nidhiprajapati1111@gmail.com>
2025-01-10 11:30:49 +0530

    add select option menu

--- hrms-web-app/src/app/component/assetsmaster/assetsmaster.component.ts
+++ hrms-web-app/src/app/component/assetsmaster/assetsmaster.component.ts
@@ -25,11 +25,13 @@ export class AssetsmasterComponent {
 
   public columnDefs: ColDef[] = [
     // { field: "id", floatingFilter: true, filter: true,},
-    { field: "assetsMasterName",headerName:'Asset Name', floatingFilter: true, filter: true,},
+    { field: "assetsMasterName", floatingFilter: true, filter: true,},
     { field: "serialNumber", floatingFilter: true, filter: true,},
+    { field: "description", floatingFilter: true, filter: true,},
     { field: "description", floatingFilter: true, filter: true,},
     { field: "dateOfPurchase", floatingFilter: true, filter: true,},
     { field: "createdDate", floatingFilter: true, filter: true,},
+    { field: "createdBy", floatingFilter: true, filter: true,},    
     { field: "createdBy", floatingFilter: true, filter: true,},    
     { field: "isActive", cellRenderer: (params: ICellRendererParams) => params.value ? `<i class="fa-solid fa-toggle-on" style="color: green; font-size: x-large;"></i>` : `'<i class="fa-solid fa-toggle-off" style="color: red; font-size: x-large;"></i>` },
     { field: "action", cellRenderer: ActionComponent, cellRendererParams: { Edit: this.Edit.bind(this), Delete: this.Delete.bind(this) } }
