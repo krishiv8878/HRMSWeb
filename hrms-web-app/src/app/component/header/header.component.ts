c2c8b982e8b2474c5a7c79955a851b8e03145a88
nidhiprajapati11 <nidhiprajapati1111@gmail.com>
2025-05-14 13:15:10 +0530

    implement auth_guard

--- hrms-web-app/src/app/component/header/header.component.ts
+++ hrms-web-app/src/app/component/header/header.component.ts
@@ -1,11 +1,11 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 // import { SidbarComponent } from "../sidbar/sidbar.component";
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 // import { RouterLink } from '@angular/router';
 import {MatMenuModule} from '@angular/material/menu';
-import {RouterLink} from '@angular/router'
+import {Router, RouterLink} from '@angular/router'
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -18,7 +18,12 @@ import { CommonModule } from '@angular/common';
 export class HeaderComponent {
   @Output() toggleSidebarForMe: EventEmitter<any> = new EventEmitter();
   constructor() { }
+  router =inject(Router)
   toggleSidebar() {
     this.toggleSidebarForMe.emit();
+  }
+  logout(){
+    localStorage.removeItem("LoginTokan");
+    this.router.navigate(['login'])
   }
 }
