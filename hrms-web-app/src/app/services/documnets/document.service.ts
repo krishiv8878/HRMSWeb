d82694d2811d1bf1cb343bb18e0a5eb2ba36317f
Nidhi prajapati <89444603+nidhiprajapati11@users.noreply.github.com>
2025-06-06 17:32:03 +0530

    Feature/user profile (#49)
    * create user profile
    
    * implement auth_guard
    
    * implement auth_guard
    
    * implement profile page
    
    * implement profile page
    
    * change user page
    
    * change role,employee service file
    
    * required change in employee document
    
    * implement profile page
    
    * required change in employee document

--- hrms-web-app/src/app/services/documnets/document.service.ts
+++ hrms-web-app/src/app/services/documnets/document.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import { Observable } from 'rxjs';
+import { catchError, Observable, tap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +15,16 @@ export class DocumentService {
   getAll() {
     return this.http.get<any[]>(this.apiUrl + `/EmployeeDocument/GetAllDocumentsInfo`)
   }
-  creatDocument(formData: FormData) {
-    return this.http.post<any[]>(this.apiUrl + `/EmployeeDocument/UploadDocument`, formData)
+  creatDocument(formData: FormData): Observable<any> {
+    const documentName = formData.get('documentName') as string;
+    debugger
+    return this.http.post<any[]>(this.apiUrl + `/EmployeeDocument/UploadDocument?documentName=${encodeURIComponent(documentName)}`, formData).pipe(
+      tap(() => console.log(' API called')),
+      catchError((error) => {
+        console.error(' Error from API service:', error);
+        return throwError(() => error);
+      })
+    )
   }
   viewDocument(id: number): Observable<any> {
     return this.http.get(`${this.apiUrl}/EmployeeDocument/view/${id}`, { responseType: 'blob' });
